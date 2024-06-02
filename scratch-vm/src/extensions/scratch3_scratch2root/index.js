8874c792a56b9feba5d56e511d62f2db6501bba5
Junya Ishihara <junya@champierre.com>
2024-06-02 15:14:11 +0900

    Add "drive distance" block.

--- scratch-vm/src/extensions/scratch3_scratch2root/index.js
+++ scratch-vm/src/extensions/scratch3_scratch2root/index.js
@@ -39,6 +39,11 @@ const Message = {
         'ja': '後ろに進む',
         'ja-Hira': 'うしろにすすむ',
         'en': 'backward'
+    },
+    driveDistance: {
+        'ja': '[DISTANCE] mm進む',
+        'ja-Hira': '[DISTANCE] mmすすむ',
+        'en': 'drive [DISTANCE] mm'
     }
 };
 
@@ -107,6 +112,17 @@ class Scratch3Scratch2RootBlocks {
                     opcode: 'disconnect',
                     blockType: BlockType.COMMAND,
                     text: Message.disconnect[this.locale]
+                },
+                {
+                    opcode: 'driveDistance',
+                    blockType: BlockType.COMMAND,
+                    text: Message.driveDistance[this.locale],
+                    arguments: {
+                        DISTANCE: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 100
+                        }
+                    }
                 },
                 {
                     opcode: 'forward',
@@ -235,6 +251,60 @@ class Scratch3Scratch2RootBlocks {
         ]);
 
         await this.rxChar.writeValue(value);
+    }
+
+    async driveDistance (args) {
+        console.log('driveDistance');
+        const distance = Cast.toNumber(args.DISTANCE);
+        const value = this.setDistance(distance);
+
+        await this.rxChar.writeValue(this.appendCrc(value));
+    }
+
+    generateCrc8Table () {
+        const polynomial = 0x07;
+        const table = new Uint8Array(256);
+        for (let i = 0; i < 256; i++) {
+            let crc = i;
+            for (let j = 0; j < 8; j++) {
+                if (crc & 0x80) {
+                    crc = (crc << 1) ^ polynomial;
+                } else {
+                    crc <<= 1;
+                }
+            }
+            table[i] = crc & 0xFF;
+        }
+        return table;
+    }
+
+    crc8 (data) {
+        let crc = 0x00; // Initial value
+        for (let i = 0; i < data.length; i++) {
+            const byte = data[i];
+            crc = this.crcTable[(crc ^ byte) & 0xFF];
+        }
+        return crc;
+    }
+
+    appendCrc (value) {
+        const newValue = new Uint8Array(value.length + 1);
+        newValue.set(value);
+        newValue[19] = this.crc8(value);
+        return newValue;
+    }
+
+
+    setDistance (distance) {
+        const arr = new Uint8Array(19);
+        arr[0] = 1;
+        arr[1] = 8;
+        arr[2] = 0;
+        arr[3] = (distance >> 24) & 0xFF;
+        arr[4] = (distance >> 16) & 0xFF;
+        arr[5] = (distance >> 8) & 0xFF;
+        arr[6] = distance & 0xFF;
+        return arr;
     }
 
     /**
